88ca27264fb04bc79782af5fc85887174a9dec4d
netdump <netdump@localhost>
2017-02-06 13:25:31 -0400

    Telnet is the proper definition

--- templates/cisco.telnet.php
+++ templates/cisco.telnet.php
@@ -1,6 +1,6 @@
 <?php
 
-$_TEMPLATE["cisco.old"] = array(
+$_TEMPLATE["cisco.telnet"] = array(
 	"cmd" => "telnet $address",
 	"cases" => $_TEMPLATE["cisco"]["cases"],
 	"answers" => $_TEMPLATE["cisco"]["answers"]
