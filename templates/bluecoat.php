4fa0c7d8c94c0168d71426875fd538fe23ca6fc6
netdump <netdump@localhost>
2017-02-16 17:25:29 -0400

    Bluecoat packet shaper template prototype

---
+++ templates/bluecoat.php
@@ -0,0 +1,71 @@
+<?php
+
+// Bluecoat PacketShaper
+//
+// https://bto.bluecoat.com/packetguide/8.7/nav/tasks/administrative/backup-configuration.htm
+// https://bto.bluecoat.com/packetguide/8.7/info/automate-backups.htm
+
+$_TEMPLATE["bluecoat"] = array(
+	"cmd" => "ssh -q -oStrictHostKeyChecking=no " . $auth[1] . "@" . $address
+	, "cases" => array(
+		array(
+			  array("^.*[Pp]assword:", "sshpassword", EXP_REGEXP, "jump")
+//			, array("^.*[-_\.0-9A-Za-z]+#", "setup", EXP_REGEXP, "jump")
+		)
+		, array(
+			array("^.*[-_\.0-9A-Za-z]+#", "ftp-backup", EXP_REGEXP, "jump")
+		)
+		, array(
+			array("^.*[-_\.0-9A-Za-z]+#", "ftp-config", EXP_REGEXP, "jump")
+		)
+		, array(
+			array("^.*[-_\.0-9A-Za-z]+#", "ftp-settings", EXP_REGEXP, "jump")
+		)
+		, array(
+			array("^.*[-_\.0-9A-Za-z]+#", "ftp-basic", EXP_REGEXP, "jump")
+		)
+		, array(
+			array("^.*[-_\.0-9A-Za-z]+#", "exit", EXP_REGEXP, "finish")
+		)
+	)
+  , "answers" => array(
+		array(
+			  array("sshpassword", "$auth[2]\n", 1)
+//			, array("setup", "setup capture complete backup.cmd\n", 1)
+		)
+    , array(
+			array("ftp-backup", 
+				"ftpput $auth[3]:$auth[4]@$auth[5] 9.256/cmd/backup.cmd ftp/".$target_tag."-backup.cmd\n"	, 1)
+		)
+    , array(
+			array("ftp-config", 
+				"ftpput $auth[3]:$auth[4]@$auth[5] 9.256/cfg/config.ldi ftp/".$target_tag."-config.ldi\n"	, 1)
+		)
+    , array(
+			array("ftp-settings", 
+				"ftpput $auth[3]:$auth[4]@$auth[5] 9.256/cfg/settings.cfg ftp/".$target_tag."-settings.cfg\n", 1)
+		)
+    , array(
+			array("ftp-basic", 
+				"ftpput $auth[3]:$auth[4]@$auth[5] 9.256/cfg/basic.cfg ftp/".$target_tag."-basic.cfg\n", 1)
+		)
+    , array(
+			array("exit", "exit\n", 3)
+		)
+	)
+	, "pre-exec" => array(
+		"rm -f '/opt/netdump/ftp/".$target_tag."-backup.cmd'"
+		,"rm -f '/opt/netdump/ftp/".$target_tag."-config.ldi'"
+		,"rm -f '/opt/netdump/ftp/".$target_tag."-settings.cfg'"
+		,"rm -f '/opt/netdump/ftp/".$target_tag."-basic.cfg'"
+	)
+	/*
+	, "post-exec" => array(
+		"rm -f '/opt/netdump/ftp/".$target_tag."-backup.cmd'"
+		,"rm -f '/opt/netdump/ftp/".$target_tag."-config.ldi'"
+		,"rm -f '/opt/netdump/ftp/".$target_tag."-settings.cfg'"
+		,"rm -f '/opt/netdump/ftp/".$target_tag."-basic.cfg'"
+	)
+	*/
+);
+
