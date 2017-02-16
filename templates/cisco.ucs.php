8f266dedbc84072c28532c18d10df3150edebb1b
netdump <netdump@localhost>
2017-02-16 14:57:24 -0400

    UCS templates tested

--- templates/cisco.ucs.php
+++ templates/cisco.ucs.php
@@ -7,6 +7,7 @@
 // http://www.cisco.com/c/en/us/td/docs/unified_computing/ucs/sw/cli/config/guide/2-1/b_UCSM_CLI_Configuration_Guide_2_1/b_UCSM_CLI_Configuration_Guide_2_1_chapter_0100111.html
 //
 // This templates works if you create a backup in the following way:
+//
 // scope system
 // create backup {protocol}://{server}/{target}.bin disabled
 // commit-buffer
@@ -17,7 +18,6 @@
 // {server}   = tftp, ftp, sftp, scp (SSH) IP server address
 // {target}   = Target tag as specified in the target.conf
 
-
 $_TEMPLATE["cisco.ucs"] = array(
 	"cmd" => "ssh -q -oStrictHostKeyChecking=no " . $auth[1] . "@" . $address
 	, "cases" => array(
@@ -27,11 +27,20 @@ $_TEMPLATE["cisco.ucs"] = array(
 			, array("^.*[-_\.0-9A-Za-z]+ \/system #", "backup", EXP_REGEXP, "jump")
 		)
 		, array(
-			  array("^.*[-_\.0-9A-Za-z]+ \/system/backup #", "enable", EXP_REGEXP)
-			, array("^.*[Pp]assword:", "password", EXP_REGEXP, "jump")
+			  array("^.*[-_\.0-9A-Za-z]+ \/system/backup\** #", "set-type", EXP_REGEXP, "jump")
 		)
 		, array(
-			  array("^.*[-_\.0-9A-Za-z]+ \/system/backup\** #", "commit", EXP_REGEXP, "jump")
+			  array("^.*[-_\.0-9A-Za-z]+ \/system/backup\** #", "set-remote-file", EXP_REGEXP, "jump")
+		)
+		, array(
+			  array("^.*[-_\.0-9A-Za-z]+ \/system/backup\** #", "enable", EXP_REGEXP, "jump")
+		)
+		// Uncomment for FTP, SFTP and SCP
+		//, array(
+		//	array("^.*[Pp]assword:", "password", EXP_REGEXP, "jump")
+		//)
+		, array(
+			  array("^.*[-_\.0-9A-Za-z]+ \/system/backup\** #", "commit-buffer", EXP_REGEXP, "jump")
 		)
 		, array(
 			  array("^.*[-_\.0-9A-Za-z]+ \/system/backup #", "exit", EXP_REGEXP)
@@ -43,14 +52,23 @@ $_TEMPLATE["cisco.ucs"] = array(
 		array(
 			  array("sshpassword", "$auth[2]\n", 1)
 			, array("system", "scope system\n", 1)
-			, array("backup", "scope backup $auth[3]\n", 1)
+			, array("backup", "scope backup $auth[4]\n", 1)
+		)
+    , array(
+			  array("set-type", "set type $auth[3]\n", 1)
+		)
+    , array(
+			  array("set-remote-file", "set remote-file " . $target_tag . ".bin\n", 1)
 		)
     , array(
 			  array("enable", "enable\n", 1)
-			, array("password", "$auth[4]\n", 1)
 		)
+		// Uncomment for FTP, SFTP and SCP
+		//, array(
+		//	array("password", "$auth[4]\n", 1)
+		//)
     , array(
-			  array("commit", "commit-buffer\n", 1)
+			  array("commit-buffer", "commit-buffer\n", 1)
 		)
     , array(
 			  array("exit", "exit\n", 3)
@@ -60,7 +78,7 @@ $_TEMPLATE["cisco.ucs"] = array(
 		"rm -f '/var/lib/tftpboot/" . $target_tag . ".bin'" // Delete any previous backup
 	)
 	, "post-exec" => array(
-		"sleep 45" // UCS asynchronous job triggering
+		"sleep 120" // UCS asynchronous job triggering
 		, "mv '/var/lib/tftpboot/" . $target_tag . ".bin' " . escapeshellarg($outfile)
 	)
 );
