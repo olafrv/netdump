7ffb8097fe09d8d84476dd265fe069f79359a005
netdump <netdump@localhost>
2017-02-16 07:55:47 -0400

    Bugfix on commiting new file changes

--- git/git.sh
+++ git/git.sh
@@ -16,9 +16,11 @@ ERROR=0
 git config --global user.email "netdump@localhost"
 git config --global user.name  "netdump"
 
+# Create repository directory (if not exists)
 [ -d "$GREPO" ] || mkdir -p "$GREPO"
 ERROR=$?
 
+# Enter and init the repository (if not initialized)
 if [ $ERROR -eq 0 ] && [ -d "$GREPO" ]
 then
 	cd "$GREPO" 
@@ -26,18 +28,33 @@ then
 	ERROR=$?
 fi
 
-if [ $ERROR -eq 0 ] && [ ! -f "$GITDF" ]
+if [ $ERROR -eq 0 ] 
 then
-	if [ ! -z "$DEBUG" ]; then echo "Initial commit: $GCOMM"; fi
-	cp "$SRCDF" "$GITDF" && git add -A && git commit -m "$GCOMM"
-	ERROR=$?
-fi
+	# First commit?
+	[ ! -f "$GITDF" ]
+	FIRST=$?
 
-if [ $ERROR -eq 0 ] && [ $(git diff | wc -l) -ne 0 ]
-then
-	if [ ! -z "$DEBUG" ]; then git diff; fi
-	git add -A && git commit -m "$GCOMM"
+	# Copy the dump to the repository
+	cp "$SRCDF" "$GITDF" 
 	ERROR=$?
+	if [ $ERROR -eq 0 ] 
+	then
+		# Is this a first commit?
+		if [ $FIRST -eq 0 ]
+		then 
+			if [ ! -z "$DEBUG" ]; then echo "$GCOMM (Initial commit)"; fi
+			git add -A && git commit -m "$GCOMM (Initial commit)"
+			ERROR=$?
+		else
+			if [ ! -z "$DEBUG" ]; then git diff; fi
+			# Not first, but are there any differences?
+			if [ $(git diff | wc -l) -ne 0 ]
+			then 	
+				git add -A && git commit -m "$GCOMM"
+				ERROR=$?
+			fi
+		fi
+	fi
 fi
 
 exit $ERROR
