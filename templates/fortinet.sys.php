fd530631e8d9df69c131d3221a35dca2bb00d63e
netdump <netdump@localhost>
2017-02-26 21:27:29 -0400

    Deprecated all template which do an incomplete backup

--- templates/fortinet.sys.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$_TEMPLATE["fortigate.sys"] = array(
-	"cmd" => "scp -q -oStrictHostKeyChecking=no $auth[1]@$address:sys_config %outfile%"
-	, "cases" => $_TEMPLATE["fortigate"]["cases"]
-	, "answers" => $_TEMPLATE["fortigate"]["answers"]
-);
