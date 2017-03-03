9c8e19719d9406b7e43b3b34b0992e513423214a
netdump <netdump@localhost>
2017-03-03 12:31:10 -0400

    Bugfix about template not commiting changes to git

--- templates/fortinet.fortios.novdom.php
+++ templates/fortinet.fortios.novdom.php
@@ -1,21 +1,19 @@
 <?php
 
-$_TEMPLATE["fortinet.fortios"] = array(
+$_TEMPLATE["fortinet.fortios.novdom"] = array(
 	"cmd" => "ssh -q -oStrictHostKeyChecking=no " . $auth[1] . "@" . $address
 	, "output" => "async"
 	, "cases" => array(
 		array(
 			  array("^.*[Pp]assword:", "sshpassword", EXP_REGEXP)
 			, array("^.*[-_\.0-9A-Za-z]+ #", "prompt", EXP_REGEXP)
-			,	array("^.*[-_\.0-9A-Za-z]+ \(global\) #", "prompt global", EXP_REGEXP)
 		)
 	)
   , "answers" => array(
 		array(
 			  array("sshpassword", "$auth[2]\n", 1)
-			, array("prompt", "config global\n", 1)
-			,	array("prompt global", "execute backup full-config ftp ftp/" . $target_tag . ".conf $auth[5] $auth[3] $auth[4]\n", 1)
-			, array("prompt global", "end\n", 1)
+			,	array("prompt", "execute backup full-config ftp ftp/" . $target_tag . ".conf $auth[5] $auth[3] $auth[4]\n", 1)
+			, array("prompt", "end\n", 1)
 			, array("prompt", "exit\n", 1)
 		)
 	)
---
+++ templates/netgear.php
@@ -0,0 +1,29 @@
+<?php
+
+$_TEMPLATE["netgear"] = array(
+	"cmd" => "telnet $address",
+	"cases" => array(
+		array(
+			array("*User Name:", "user", EXP_GLOB),
+			array("*Password:", "password", EXP_GLOB),
+			array("^.*[-_\.0-9A-Za-z]+#", "prompt", EXP_REGEXP, "jump")
+		),
+		array(
+			array("*\n", "save", EXP_GLOB),
+			array("More: <space>,*", "more", EXP_GLOB),
+			array("^[-_\.0-9A-Za-z]+#$", "exit", EXP_REGEXP, "finish")
+		)
+	),
+	"answers" => array(
+		array(
+			array("user", "$auth[1]\n", 1),
+			array("password", "$auth[2]\n", 1),
+			array("prompt", "show running-config\n", 1)
+		),
+		array(
+			array("more", " ", -1),
+			array("exit", "exit\n", 1)
+		)
+	)
+);
+
